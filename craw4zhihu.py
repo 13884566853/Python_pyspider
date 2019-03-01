e53ead4978f1d13c0499a41b8a66a14c089badbf
13884566853 <838924569@qq.com>
2019-03-02 03:44:24 +0800

    Update craw4zhihu.py

--- craw4zhihu.py
+++ craw4zhihu.py
@@ -8,16 +8,20 @@ import random
 import MySQLdb
 
 class Handler(BaseHandler):
+    #基础配置
     crawl_config = {
         'itag': 'v1',
         'headers': {
+            #伪装成Google爬虫
             'User-Agent': 'GoogleBot',
             'Host' : 'www.zhihu.com',
             'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         }
     }
+    #配置数据库基本参数
     def __init__(self):
         self.db = MySQLdb.connect('localhost', 'root', 'root', 'pyspider', charset='utf8')
+    #将爬取结果插入数据库
     def add_question(self, title, content):
         try:
             CURSOR = self.db.cursor()
@@ -30,23 +34,27 @@ class Handler(BaseHandler):
             print e
             self.db.rollback()
         return 0
-            
+    #每天执行一次        validate_cert=False是省略ssl安全检查
     @every(minutes=24 * 60)
     def on_start(self):
         self.crawl('https://www.zhihu.com/topic/19561132/top-answers', callback=self.index_page,validate_cert=False)
-
+    #十天以内不重复爬
     @config(age=10 * 24 * 60 * 60)
     def index_page(self, response):
+        #之前页面选取的爬取的条目 这里用css选择器写法 注意的是留空格的class样式 类选择器中间有空格代表后代选择，这里写法是同级，所以把空格换成点
         for each in response.doc('a[data-za-detail-view-element_name="Title"]').items():
             self.crawl(each.attr.href, callback=self.detail_page, validate_cert=False)
 
     @config(priority=2)
     def detail_page(self, response):
         items = response.doc('div.RichText.ztext').items()
+        #detal页面的话题标题
         title=response.doc('h1.QuestionHeader-title').text()
+        #话题内容
         html=response.doc('span.RichText.ztext.CopyrightRichText-richText').html()
         if html==None:
             html=''
+        #避免转义错误 合法的规则插入数据库
         html=html.replace('"', '\\"')
         qid=self.add_question(title, html)
         return {
